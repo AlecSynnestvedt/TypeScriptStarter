15e641fcf84f0b8bc8180f925f731327514ae9da
Alec Synnestvedt <aleccsyn@gmail.com>
2021-03-23 14:44:15 -0400

    more updates

--- app/01-types.ts
+++ app/01-types.ts
@@ -115,3 +115,105 @@ Enums are a feature added to JavaScript by TypeScript which allows for describin
 enum CardType { Ace = 1, Two, Three, Four }
 let CardType: string = CardType[2]
 console.log(cardType);
+
+
+/* 
+UNION TYPES
+___________________
+Union types allow us to have more flexibility with our variables, but we still keep the intent of declaring a specific type for our variables.
+
+consider the code: 
+*/
+let x: number | string; 
+let y: number | null; 
+//now, the variables can take on more than one type: 
+x = '1234';
+x = 1234;
+//these throw an error.
+x = true; 
+y = false;
+
+/*
+Defining a Union Type
+The first way to combine types you might see is a union type. A union type is type formed from two or more other types, representing values that may be any one of those types. We refer to each of these types as the union’s members.
+
+Let’s write a function that can operate on strings or numbers.
+*/
+
+function printId(id: number | string) {
+  console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
+// Error
+printId({ myID: 22342 });
+// Argument of type '{ myID: number; }' is not assignable to parameter of type 'string | number'.
+//   Type '{ myID: number; }' is not assignable to type 'number'.
+/*
+TypeScript will only allow you to do things with the union if that thing is valid for every member of the union. For example, if you have the union string | number, you can’t use methods that are only available on string:
+*/
+function printId(id: number | string) {
+  console.log(id.toUpperCase());
+// Property 'toUpperCase' does not exist on type 'string | number'.
+//   Property 'toUpperCase' does not exist on type 'number'.
+}
+/*
+The solution is to narrow the union with code, the same as you would in JavaScript without type annotations. Narrowing occurs when TypeScript can deduce a more specific type for a value based on the structure of the code.
+
+For example, TypeScript knows that only a string value will have a typeof value "string":
+*/
+
+function printId(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+/*
+Another example is to use a function like Array.isArray:
+*/
+function welcomePeople(x: string[] | string) {
+  if (Array.isArray(x)) {
+    // Here: 'x' is 'string[]'
+    console.log("Hello, " + x.join(" and "));
+  } else {
+    // Here: 'x' is 'string'
+    console.log("Welcome lone traveler " + x);
+  }
+}
+/*
+Notice that in the else branch, we don’t need to do anything special - if x wasn’t a string[], then it must have been a string.
+
+Sometimes you’ll have a union where all the members have something in common. For example, both arrays and strings have a slice method. If every member in a union has a property in common, you can use that property without narrowing:
+*/
+// Return type is inferred as number[] | string
+function getFirstThree(x: number[] | string) {
+  return x.slice(0, 3);
+}
+
+/* 
+TYPESCRIPT SAFETY IN ARGUMENTS 
+++++++++++++++++++++++++++++++
+
+TypeScript allows us to bring types to our parameters and limits to the number of arguments we pass in. Go into the  file. Let's add some functions, and add the following code:
+*/
+function addNumbers(numOne: number, numTwo: number){ 
+  return numOne + numTwo; 
+}
+
+//1 - Works
+addNumbers(1, 2);
+
+//2 - Errors
+addNumbers(1, 2, 3);
+addNumbers(1, 2, 3, "Foo");
+
+//Practice...
+function fullName(firstName: string, lastName: string){
+  return firstName + lastName;
+}
